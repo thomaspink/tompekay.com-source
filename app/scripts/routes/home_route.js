9f8a9c1a3315c1633d2465e0b3b2dfbf972d9034
Thomas <thomas.pink@me.com>
2014-03-25 20:53:04 +0100

    contact fixes, home videos from contentful api

--- app/scripts/routes/home_route.js
+++ app/scripts/routes/home_route.js
@@ -1,5 +1,19 @@
 TompekayCom.HomeRoute = Ember.Route.extend({
     model: function() {
-        return TompekayCom.contentfulClient.entry('5Sg2kDcFhuuiY4KyY2OAMa');
+        return TompekayCom.contentfulClient.entry('5Sg2kDcFhuuiY4KyY2OAMa').then(function(data) {
+            if(!!data && !!data.fields && !!data.fields.videoMp4) {
+                return TompekayCom.contentfulClient.asset(data.fields.videoMp4.sys.id).then(function(dataMp4) {
+                    if(!!data.fields.videoOgv) {
+                        return TompekayCom.contentfulClient.asset(data.fields.videoOgv.sys.id).then(function(dataOgv) {
+                            return [dataMp4.fields, dataOgv.fields];
+                        });
+                    } else {
+                        return dataMp4.fields;
+                    }
+                });
+            } else {
+                return {};
+            }
+        });
     }
 });
